c3e0efa9cded907b3f935ea3e3932e18edc05360
skanjaruek <tanapattara@kku.ac.th>
2022-11-28 17:39:15 +0700

    add weight

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Student
+{
+    public string name;
+    public int weight;
+
+    public Student(string modelName, int modelWeight)
+    {
+        name = modelName;
+        weight = modelWeight;
+
+    }
+    static void Main(string[] args)
+    {
+        Student student1 = new Student("Pe", 62);
+        Student student2 = new Student("haroon", 90);
+        Student student3 = new Student("Ter", 60);
+        Student student4 = new Student("palm", 55);
+        Student student5 = new Student("Ley", 62);
+        int[] weightlist = { student1.weight, student2.weight, student3.weight, student4.weight, student5.weight };
+        int Max = weightlist.Max();
+        int Min = weightlist.Min();
+        Console.WriteLine("ค่าเฉลี่ยน้ำหนักของนักศึกษาทั้งหมด" + (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5);
+        Console.WriteLine("ชื่อและน้ำหนักของคนที่มีน้ำหนักมากที่สุด" + Max);
+        Console.WriteLine("ชื่อและน้ำหนักของคนที่มีน้ำหนักน้อยที่สุด" + Min);
+        Console.WriteLine("นาย พิชัย ทองอาสา 653450096-4");
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Student
+{
+    public string name;
+    public int weight;
+
+    public Student(string modelName,int modelWeight)
+    {
+        name = modelName;
+        weight = modelWeight;
+    }
+    static void main(string[])
+    {
+        Student student1 = new Student("Pe", 62);
+        Student student2= new Student("Top", 79);
+        Student student3= new Student("Ter", 70);
+        Student student4 = new Student("Ley", 90);
+        Student student5 = new Student("Haroon", 82);
+        int[] weightList = { student1.weight, student2.weight, student3.weight, student4.weight, student5.weight };
+        int max = weightList.Max();
+        int min = weightList.Max();
+        Console.WriteLine("แสดงค่าเฉลี่ยน้ำหนักของนักศึกษาทั้งหมด" + (student1.weight, student2.weight, student3.weight, student4.weight, student5.weight) / 5);
+        Console.WriteLine("แสดงชื่อและน้ำหนักของคนที่มีน้ำหนักมากที่สุด" + max);
+        Console.WriteLine("แสดงชื่อและน้ำหนักของคนที่มีน้ำหนักน้อยที่สุด" + min);
+        Console.WriteLine("นาย พิชัย ทองอาสา 653450096ข4");
+    }
+}
